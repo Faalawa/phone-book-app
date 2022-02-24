10564f82815b66b707ef328ffc89aef6f8c07d7f
Awa <awafall1256@gmail.com>
2022-02-24 12:13:59 -0500

    contact information

--- phonebook.py
+++ phonebook.py
@@ -1,1 +1,8 @@
 selected_option = input("what do you want to do(1-5)?")
+if selected_option == "2":
+    name =input("what is the contact's name?")
+    phone_number = input("what is their phone number?")
+    phonebook[name] = phone_number
+    print("contact added successfully!")
+    print(menu)
+    
